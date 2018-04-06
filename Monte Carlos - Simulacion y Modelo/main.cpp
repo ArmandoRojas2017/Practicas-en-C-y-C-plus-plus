65c8ef911599d09c3e802159270851e79af5dd57
Armando Rojas <rojasarmando260@gmail.com>
2018-04-05 23:41:26 -0400

    cambiando archivo base

---
+++ Monte Carlos - Simulacion y Modelo/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+int main(){
+	
+	
+	cout<<"\t\t\t##############################"<<endl;
+	cout<<"\t\t\t#   Metodo de Monte Carlos   #"<<endl;
+	cout<<"\t\t\t#   By: Armando Rojas        #"<<endl;
+	cout<<"\t\t\t##############################"<<endl;
+	
+	return 2;
+}
